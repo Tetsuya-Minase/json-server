87ae4b045d44e4fa0d0bc0157f730115ed04e8df
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2019-12-19 23:48:08 +0900

    feat: header component.

---
+++ src/app/common/component/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { LinkObject } from '../../../model/objects/link.object';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  readonly LINK_LIST: LinkObject[] = [
+    {
+      text: '編集',
+      url: '/edit'
+    },
+    {
+      text: '一覧',
+      url: '/list'
+    },
+  ];
+}
