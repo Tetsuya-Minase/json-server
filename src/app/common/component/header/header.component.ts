342c4768b4bc31e3aa2de51b0069c239feea627c
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-03-04 22:26:50 +0900

    style: update header css.🐔

--- src/app/common/component/header/header.component.ts
+++ src/app/common/component/header/header.component.ts
@@ -1,24 +1,8 @@
 import { Component } from '@angular/core';
-import { LinkObject } from '../../../model/objects/link.object';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-  readonly LINK_LIST: LinkObject[] = [
-    {
-      text: '編集',
-      url: '/edit',
-    },
-    {
-      text: '一覧',
-      url: '/list',
-    },
-  ];
-  readonly TOP_LINK: LinkObject = {
-    text: 'JSON-SERVER',
-    url: '/',
-  };
-}
+export class HeaderComponent {}
