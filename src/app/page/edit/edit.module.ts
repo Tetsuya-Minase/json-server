c27e3a56d2156c76262e05c975a7be70d801f135
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-03-06 23:57:20 +0900

    style: update edit page style.🎨

--- src/app/page/edit/edit.module.ts
+++ src/app/page/edit/edit.module.ts
@@ -6,9 +6,10 @@ import { EditRoutingModule } from './edit-routing.module';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { FormErrorComponent } from '../../common/component/form-error/form-error.component';
 import { EditService } from './service/edit.service';
+import { ButtonComponent } from '../../common/atoms/button/button.component';
 
 @NgModule({
-  declarations: [EditComponent, FormErrorComponent],
+  declarations: [EditComponent, FormErrorComponent, ButtonComponent],
   imports: [
     CommonModule,
     FormsModule,
